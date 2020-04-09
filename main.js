1c2f5d67483c9de8631b3d19e20fbf03467dcfcc
Wilson Chen <wilson.chen@codeimmersives.com>
2020-04-09 15:35:19 -0400

    finish

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
 class Vehicle {
+  constructor(name,wheel,maxSpeed){
+    this.name = name
+    this.wheelCount = wheel
+    this.maxSpeed = maxSpeed
+    this.speed = 0
+  }
+  accelerate(){
+    if(this.speed < this.maxSpeed){
+    this.speed += this.maxSpeed/3}
+  }
 }
 
-class Car {
+class Car extends Vehicle{
+  constructor(name,carSpeed){
+    super(name,4,carSpeed)
+    Car.maxSpeed = carSpeed
+  }
+  drive(){
+    return 'Vroom. '
+  }
 }
 
-class Ferrari {
+class Ferrari extends Car{
+  constructor(){
+    super('Ferrari',180)
+  }
+  drive(){
+    return super.drive() + `I'm a Ferrari!`
+  }
+  accelerate(){
+    if(this.speed < this.maxSpeed){
+      this.speed += this.maxSpeed/2}
+  }
 }
+
 
 
 module.exports = {
